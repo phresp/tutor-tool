715ad1aa55ade401f82f7edc9a894b76e761f0e4
Philipp Spanner <philipp.spanner@googlemail.com>
2020-11-21 18:03:40 +0100

    Delete, see single Semester and edit Semester in Backend

--- routes/api/semester.js
+++ routes/api/semester.js
@@ -12,11 +12,11 @@ const validateSemesterInput = require("../../validation/semester");
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Semester Works" }));
 
-// @route   GET api/semester/all
+// @route   GET api/semester/
 // @desc    Get all Semester
 // @access  Private
 router.get(
-  "/all",
+  "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
     const errors = {};
@@ -34,6 +34,20 @@ router.get(
   }
 );
 
+// @route   GET /api/semester/:id
+// @desc    Get Semester by id
+// @access  Private
+router.get("/:id", (req, res) => {
+  const errors = {};
+  Semester.findOne({ _id: req.params.id })
+    .then((semester) => {
+      res.json(semester);
+    })
+    .catch((err) =>
+      res.status(404).json({ semesternotfound: "Semester not found" })
+    );
+});
+
 // @route   POST /api/semester
 // @desc    Create or Update Semester
 // @access  Private
@@ -50,7 +64,7 @@ router.post(
     //Check if Semester Name is already there
     Semester.findOne({ name: req.body.name.toUpperCase() }).then((semester) => {
       if (semester) {
-        errors.profile = "Semester already exists";
+        errors.name = "Semester already exists";
         return res.status(400).json(errors);
       } else {
         //If semester does not exist create
@@ -68,6 +82,67 @@ router.post(
           .then((semester) => res.json(semester));
       }
     });
+  }
+);
+
+// @route   POST api/semester/:id
+// @desc    Edit Semester
+// @access  Private
+router.post(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateSemesterInput(req.body);
+    //Check validation
+    if (!isValid) {
+      //If not valid, send 400 with errors
+      return res.status(400).json(errors);
+    }
+
+    Semester.findOne({ name: req.body.name.toUpperCase() }).then((semester) => {
+      if (semester && semester._id != req.params.id) {
+        //Above expression works but is not really nice
+        errors.name = "Semester already exists";
+        return res.status(400).json(errors);
+      } else {
+        //Get Body Fields
+        const semesterFields = {};
+        semesterFields.name = req.body.name.toUpperCase();
+        semesterFields.from = req.body.from;
+        semesterFields.to = req.body.to;
+        semesterFields.coursefrom = req.body.coursefrom;
+        semesterFields.courseto = req.body.courseto;
+
+        //Update
+        Semester.findOneAndUpdate(
+          { _id: req.params.id },
+          { $set: semesterFields },
+          { new: true }
+        )
+          .then((semester) => res.json(semester))
+          .catch((err) =>
+            res.status(404).json({ nosemesterfound: "Semester not found" })
+          );
+      }
+    });
+  }
+);
+
+// @route   DELETE /api/semester/:id
+// @desc    DELETE Semester
+// @access  Private
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Semester.findById(req.params.id)
+      .then((semester) => {
+        // Delete
+        semester.remove().then(() => res.status(200).json({ success: true }));
+      })
+      .catch((err) => {
+        res.status(404).json({ semesternotfound: "Semester not found" });
+      });
   }
 );
 
