50b8389c27b325f16ecd480ec4f88ec25e4d96f3
Philipp Spanner <philipp.spanner@googlemail.com>
2020-12-11 23:05:54 +0100

    contract validation created, create contract added and get bugs fixed

--- routes/api/contract.js
+++ routes/api/contract.js
@@ -4,6 +4,8 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 
+const validateContractInput = require("../../validation/contract");
+
 //Load Application Model
 const Application = require("../../models/Application");
 
@@ -43,7 +45,8 @@ router.get(
         },
       })
       .populate({ path: "application" })
-      .populate({ path: "user", select: { email: 1 } })
+      .populate({ path: "users", select: { email: 1 } })
+      .populate({ path: "profile" })
       .then((contracts) => {
         if (!contracts) {
           errors.nocontract = "There are no contracts yet";
@@ -76,17 +79,122 @@ router.get(
         },
       })
       .populate({ path: "application" })
-      .populate({ path: "user", select: { email: 1 } })
-      .then((applications) => {
-        if (!applications) {
-          errors.application = "There are no applications";
+      .populate({ path: "users", select: { email: 1 } })
+      .populate({ path: "profile" })
+      .then((contracts) => {
+        if (!contracts) {
+          errors.application = "There are no contracts";
           return res.status(404).json(errors);
         }
-        res.json(applications);
+        res.json(contracts);
       })
       .catch((err) =>
-        res.status(404).json({ application: "There are no applications" })
+        res.status(404).json({ application: "There are no contracts" })
       );
+  }
+);
+
+// @route   GET api/contract/contract/:id
+// @desc    Get contract by ID
+// @access  Private
+router.get(
+  "/contract/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+    Contract.findOne({ _id: req.params.id })
+      .populate({
+        path: "course",
+        select: { semester: 1, metacourse: 2 },
+        populate: {
+          path: "semester",
+        },
+        populate: {
+          path: "metacourse",
+          select: { name: 1, abbreviation: 2, module: 3 },
+        },
+      })
+      .populate({ path: "application" })
+      .populate({ path: "user", select: { email: 1 } })
+      .then((contract) => {
+        if (!contract) {
+          errors.application = "There is no contract";
+          return res.status(404).json(errors);
+        }
+        res.json(contract);
+      })
+      .catch((err) => {
+        res.status(404).json({ contract: "There is no contract with this ID" });
+      });
+  }
+);
+
+// @route   POST /api/contract
+// @desc    POST to create contract
+// @access  Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    //Validate Input fields
+    const { errors, isValid } = validateContractInput(req.body);
+    if (!isValid) {
+      //Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
+    //Get Body Fields
+    const contractFields = {};
+    contractFields.user = req.user.id;
+    contractFields.profile = req.body.profile;
+    contractFields.course = req.body.course;
+    contractFields.application = req.body.application;
+    contractFields.contractstart = req.body.contractstart;
+    contractFields.contractend = req.body.contractend;
+    contractFields.hours = req.body.hours;
+    contractFields.degree = req.body.degree;
+    contractFields.newcontract = req.body.newcontract;
+    contractFields.merkblatt = req.body.merkblatt
+      ? req.body.merkblatt
+      : "fehlt";
+    contractFields.einstellungsvorschlag = req.body.einstellungsvorschlag
+      ? req.body.einstellungsvorschlag
+      : "fehlt";
+    contractFields.versicherungspflicht = req.body.versicherungspflicht
+      ? req.body.versicherungspflicht
+      : "fehlt";
+    contractFields.scientology = req.body.scientology
+      ? req.body.scientology
+      : "fehlt";
+    contractFields.verfassungstreue = req.body.verfassungstreue
+      ? req.body.verfassungstreue
+      : "fehlt";
+    contractFields.immatrikulationsbescheinigung = req.body
+      .immatrikulationsbescheinigung
+      ? req.body.immatrikulationsbescheinigung
+      : "fehlt";
+    contractFields.aufenthaltstitel = req.body.aufenthaltstitel
+      ? req.body.aufenthaltstitel
+      : "fehlt";
+    contractFields.krankenkassenbescheinigung = req.body
+      .krankenkassenbescheinigung
+      ? req.body.krankenkassenbescheinigung
+      : "fehlt";
+    contractFields.personalbogenbezuegestelle = req.body
+      .personalbogenbezuegestelle
+      ? req.body.personalbogenbezuegestelle
+      : "fehlt";
+    contractFields.personalbogenstudierende = req.body.personalbogenstudierende
+      ? req.body.personalbogenstudierende
+      : "fehlt";
+    contractFields.sozialversicherungsausweis = req.body
+      .sozialversicherungsausweis
+      ? req.body.sozialversicherungsausweis
+      : "fehlt";
+    contractFields.steuerId = req.body.steuerId ? req.body.steuerId : "fehlt";
+
+    //Create Contract
+    new Contract(contractFields).save().then((contract) => res.json(contract));
   }
 );
 
