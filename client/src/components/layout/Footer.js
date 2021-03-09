30aec1a6ea195ca0b3d1760a62d52c1c241c0062
Philipp Spanner <philipp.spanner@googlemail.com>
2021-03-09 18:50:27 +0100

    Footer at bottom

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const MyComponent = () => {
   return (
-    <footer className="bg-dark text-white mt-5 p-4 text-center">
+    <footer className="bg-dark text-white mt-5 p-4 text-center footer">
       Copyright &copy; {new Date().getFullYear()} Tutor-Tool{" - "}
       <a href="https://www.in.tum.de/impressum/" className={"text-white"}>
         Impressum
