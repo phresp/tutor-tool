1dbf82d03880fbbfe3e88d37039a7edd825b8e1a
Philipp Spanner <philipp.spanner@googlemail.com>
2021-03-27 02:27:14 +0100

    Budget Overview SemesterSelect and Contract number

--- client/src/components/budgetcontrol/BudgetOverview.js
+++ client/src/components/budgetcontrol/BudgetOverview.js
@@ -1,30 +1,87 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import { withRouter, Link } from "react-router-dom";
 import { getCourses } from "../../actions/courseActions";
+import { getSemesters } from "../../actions/semesterActions";
+import { getContracts } from "../../actions/contractActions";
 import BootstrapTable from "react-bootstrap-table-next";
 import ToolkitProvider, { Search } from "react-bootstrap-table2-toolkit";
-import { Link } from "react-router-dom";
+import moment from "moment";
+
+import { TutorAdminDataExport } from "../../actions/formsActions";
+import SelectListGroup from "../common/SelectListGroup";
+
+const { SearchBar } = Search;
 
 class BudgetOverview extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fil: "0",
+      semesters: "",
+      semester: "",
+      contracts: "",
+      errors: {},
+    };
+
+    this.onChange = this.onChange.bind(this);
+  }
+
   componentWillMount() {
     this.props.getCourses();
+    this.props.getSemesters();
+    this.props.getContracts();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+
+    if (nextProps.semester) {
+      const { semesters } = nextProps.semester;
+      this.setState({
+        semesters: semesters,
+      });
+    }
+
+    if (nextProps.contract) {
+      const { contracts } = nextProps.contract;
+      this.setState({
+        contracts: contracts,
+      });
+    }
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   render() {
-    //Coursename and Semester
-    var coursename;
-    var coursesem;
-    if (this.props.course) {
-      if (this.props.course.course) {
-        if (this.props.course.course.metacourse.name) {
-          coursename = this.props.course.course.metacourse.name;
-        }
-        if (this.props.course.course.semester.name) {
-          coursesem = this.props.course.course.semester.name;
-        }
-      }
+    const { courses } = this.props.course;
+    var { semesters, contracts, errors } = this.state;
+    const entries = courses ? courses : [];
+
+    //Select options for semester
+    if (!semesters) {
+      semesters = [];
     }
+    const semesterOptions = semesters.map((el) => {
+      return { label: el.name, value: el._id };
+    });
+    semesterOptions.unshift({ label: "Semester auswählen", value: "" });
+
+    const defaultSorted = [
+      {
+        dataField: "status",
+        order: "desc",
+      },
+    ];
+
+    var courseArray = entries.filter((el) => {
+      if (this.state.semester === el.semester[0]._id) return el;
+    });
 
     const diff_weeks = (dt2, dt1) => {
       var diff = (dt2.getTime() - dt1.getTime()) / 1000;
@@ -32,99 +89,109 @@ class BudgetOverview extends Component {
       return Math.abs(Math.round(diff));
     };
 
-    var numberContracts = 0;
-    var overallcontracthours = 0;
-    var overallcontractweeklyhours = 0;
-    if (this.props.contract.contracts) {
-      var { contracts } = this.props.contract;
-      numberContracts = contracts.length;
-      contracts.forEach((element) => {
-        //Wochenlänge berechnen und das mal Stunden in den Wochen
-        var weeks1 = 0;
-        var weeks2 = 0;
-        if (element.contractstart && element.contractend) {
-          weeks1 = diff_weeks(
-            new Date(element.contractstart),
-            new Date(element.contractend)
-          );
-        }
+    const numberApplications = (cell, row, rowIndex, formatExtraData) => {
+      if (row.applications) {
+        return row.applications.length;
+      }
+    };
 
-        if (element.contractstart2 && element.contractend2) {
-          weeks2 = diff_weeks(
-            new Date(element.contractstart2),
-            new Date(element.contractend2)
-          );
-        }
-        var hours = element.hours ? element.hours : 0;
-        var hours2 = element.hours2 ? element.hours : 0;
+    const numberAccepted = (cell, row, rowIndex, formatExtraData) => {
+      if (row.applications) {
+        return row.applications.filter((x) => x.status == "Accepted").length;
+      }
+    };
 
-        var allhours = weeks1 * hours + weeks2 * hours2;
-        overallcontracthours += allhours;
-        overallcontractweeklyhours += hours;
+    const numberVertrag = (cell, row, rowIndex, formatExtraData) => {
+      var counter = 0;
+      contracts.forEach((e) => {
+        if (e.course._id === row._id) {
+          counter += 1;
+        }
       });
-    }
+      return counter;
+    };
 
-    //Numbers for the course
-    if (this.props.course.course) {
-      var { overallweeklyhours, overallhours } = this.props.course.course;
-    }
-
-    //className for Table
-    var tableClassName = "table";
-    var hoursTooltipp;
-    if (overallcontracthours > overallhours) {
-      hoursTooltipp = (
-        <h3 className={"text-danger text-center"}>Vertragsstunden zu hoch</h3>
+    const courseFormatter = (value, row, rowIndex, formatExtraData) => {
+      return (
+        <Link to={`/course-applications/${row._id}`} className={"text"}>
+          {value}
+        </Link>
       );
-      tableClassName = "table table-danger";
-    }
+    };
+
+    const columns = [
+      {
+        dataField: "metacourse[0].name",
+        text: "Veranstaltung",
+        sort: true,
+        formatter: courseFormatter,
+      },
+      {
+        text: "Anzahl Verträge",
+        sort: true,
+        formatter: numberVertrag,
+      },
+    ];
 
     return (
       <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <Link to={`/course-overview`} className={"btn btn-light"}>
-              back
-            </Link>
-            <h1 className="display-4 text-center">Budgetübersicht</h1>
-            {hoursTooltipp}
-            <table className={tableClassName}>
-              <thead>
-                <tr>
-                  <th scope="col">Anzahl Verträge</th>
-                  <th scope="col">Verträge Gesamtstunden</th>
-                  <th scope="col">Veranstaltung Gesamtstunden</th>
-                  <th scope="col">Verträge Gesamtwochenstunden</th>
-                  <th scope="col">Veranstaltung Gesamtwochenstunden</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>{numberContracts}</td>
-                  <td>{overallcontracthours}</td>
-                  <td>{overallhours}</td>
-                  <td>{overallweeklyhours}</td>
-                  <td>{overallcontractweeklyhours}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+        <Link to={"/course-overview"} className={"btn btn-light"}>
+          back
+        </Link>
+        <h1 className="display-4 text-center">Budget Übersicht</h1>
+        <div className={"col-md-4"}>
+          <label htmlFor="inputStudent">Semester auswählen</label>
+          <SelectListGroup
+            placeholder="Semester"
+            onChange={this.onChange}
+            value={this.state.semester}
+            name="semester"
+            error={errors.semester}
+            options={semesterOptions}
+          />
         </div>
+        <h6></h6>
+        <ToolkitProvider
+          bootstrap4
+          keyField="id"
+          data={courseArray}
+          columns={columns}
+          search
+        >
+          {(props) => (
+            <div>
+              <hr />
+              <BootstrapTable
+                {...props.baseProps}
+                striped
+                defaultSorted={defaultSorted}
+              />
+            </div>
+          )}
+        </ToolkitProvider>
       </div>
     );
   }
 }
 
 BudgetOverview.propTypes = {
-  getCourseById: PropTypes.func.isRequired,
-  application: PropTypes.object.isRequired,
+  getCourses: PropTypes.func.isRequired,
   course: PropTypes.object.isRequired,
+  semester: PropTypes.object.isRequired,
+  contract: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-  application: state.application,
   course: state.course,
+  semester: state.semester,
   contract: state.contract,
+  auth: state.auth,
 });
 
-export default connect(mapStateToProps, { getCourses })(BudgetOverview);
+export default connect(mapStateToProps, {
+  getCourses,
+  getSemesters,
+  TutorAdminDataExport,
+  getContracts,
+})(withRouter(BudgetOverview));
