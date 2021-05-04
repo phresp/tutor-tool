3b044367fddbfa739a2ab8ff453e9e40b60bc6df
Philipp Spanner <philipp.spanner@googlemail.com>
2021-05-04 15:26:40 +0200

    Changed Cred dev

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongoURI: "mongodb://localhost:27017/tutortool",
   secretOrKey: "secret",
-  mailuser: "please insert your TUM Mail ID",
-  mailsecret: "please insert your TUM secret",
+  mailuser: "please insert your in.tum Mail ID",
+  mailsecret: "please insert your in.tum secret",
 };
