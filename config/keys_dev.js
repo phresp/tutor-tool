2fb3e275118d2bf3e243f8d8eeb9092d4412c70d
Philipp Spanner <philipp.spanner@googlemail.com>
2021-05-04 14:44:07 +0200

    Changed Mail Cred for Dev

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,6 @@
 module.exports = {
   mongoURI: "mongodb://localhost:27017/tutortool",
   secretOrKey: "secret",
-  mailsecret: "psst",
+  mailuser: "please insert your TUM Mail ID",
+  mailsecret: "please insert your TUM secret",
 };
