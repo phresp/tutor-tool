96913b1dfd663a23cbe3b278497347d832cd11ac
Philipp Spanner <philipp.spanner@googlemail.com>
2021-05-14 00:41:34 +0200

    Passport prep

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
+const SamlStrategy = require("passport-saml").Strategy;
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
 const keys = require("./keys");
@@ -21,4 +22,24 @@ module.exports = (passport) => {
         .catch((err) => console.log(err));
     })
   );
+
+  //Strategy Preparation TUM Shibboleth
+  // passport.use(
+  //   new SamlStrategy(
+  //     {
+  //       path: "/login/callback",
+  //       entryPoint:
+  //         "https://openidp.feide.no/simplesaml/saml2/idp/SSOService.php",
+  //       issuer: "passport-saml",
+  //     },
+  //     function (profile, done) {
+  //       findByEmail(profile.email, function (err, user) {
+  //         if (err) {
+  //           return done(err);
+  //         }
+  //         return done(null, user);
+  //       });
+  //     }
+  //   )
+  // );
 };
